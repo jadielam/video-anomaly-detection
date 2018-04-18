f0dcc09d51d2ed7c2c0f7043f90c8ecb4fbfe0de
Jadiel deArmas <Jadiel.X.De.Armas.-ND@disney.com>
2018-04-18 15:28:07 -0400

    First commit. Added a first draft of a video anomaly detection model

---
+++ code/video_dl/model.py
@@ -0,0 +1,112 @@
+from functools import partial
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.models
+import torch.utils.model_zoo
+from torch.autograd import Variable
+use_cuda = torch.cuda.is_available()
+
+class Classifier(Module):
+    '''
+    Standard multilayer perceptron classifier.
+    '''
+    def __init__(self, nb_classes, input_size, nb_layers = 3,
+                classifier_function = partial(F.log_softmax, dim = 1),
+                activation_function = F.relu, 
+                hidden_dimension = 1024,
+                dropout = 0.2):
+        '''
+        Arguments:
+        - nb_classes (int): number of classes for classification
+        - input_size (int): length of the input vector
+        - nb_layers (int): number of layers for the classifier
+        - classifier_function: (:obj:`torch.nn.functional`) A function to apply to the last layer.
+                             Usually will be softmax or sigmoid
+        '''
+        super(Classifier, self).__init__()
+        self.classifier_function = classifier_function
+        self.activation_function = activation_function
+
+        dims = []
+        for i in range(max(nb_layers, 1)):
+            dim_entry = [hidden_dimension, hidden_dimension]
+            if i == 0: 
+                dim_entry[0] = input_size
+            if i == max(nb_layers, 1) - 1:
+                dim_entry[1] = nb_classes
+            dims.append(dim_entry)
+
+        self.dropout_layer = nn.Dropout(p = dropout)
+        self.layers = []
+        for i in range(len(dims)):
+            linear_layer = nn.Linear(dims[i][0], dims[i][1])
+            self.layers.append(linear_layer)
+            super(Classifier, self).add_module("linear_{}".format(i), linear_layer)
+
+    def forward(self, input_t):
+        '''
+        Arguments:
+        - input_t_l (:obj:`torch.Tensor`): input tensor to use for classification of size (batch, input_size)
+
+        Returns:
+        - output (:obj:`torch.Tensor`) of size (batch, nb_classes)
+        '''
+        next_t = input_t
+        next_t = self.dropout_layer(next_t)
+        for i in range(len(self.layers) - 1):
+            next_t = self.activation_function(self.layers[i](next_t))
+        next_t = self.layers[len(self.layers) - 1](next_t)
+        output = self.classifier_function(next_t)
+        return output
+
+class Identity(nn.Module):
+    def __init__(self):
+        super(Identity, self).__init__()
+
+    def forward(self, input_t):
+        return input_t
+
+class AnomalyModel(nn.Module):
+
+    def __init__(self, output_dim, gru_dropout):
+        super(AnomalyModel, self).__init__()
+        #1. Get a vgg16 or something model from torchvision
+        self._output_dim = output_dim
+        self._hidden_size = output_dim
+        self._gru_dropout = gru_dropout
+
+        self._vision_features = torchvision.models.resnet50(pretrained = True)
+        self._vision_features.fc = Identity()
+        
+        #TODO: FIgure out the size of the output of vision_features
+        #4. Create RNN module
+        self._gru_cell = nn.GRUCell(self._output_dim, self._hidden_size, bias = True)
+        
+        self._classifier = Classifier(2, self._hidden_size)
+        self._criterion = nn.NLLLoss()
+
+        #5. Store previous hidden layer here.
+        if use_cuda:
+            self._previous_hidden = Variable(torch.zeros(1, self._hidden_size)).cuda()
+        else:
+            self._previous_hidden = Variable(torch.zeros(1, self._hidden_size))
+        
+    def loss(self, input_t, ground_truth):
+        classification = self.forward(input_t)
+        loss = self.criterion(classification, ground_truth)
+        return loss
+    
+    def forward(self, input_t):
+        features = self._vision_features(input_t)
+        new_hidden = self._gru_cell(features, self._previous_hidden)
+        self._previous_hidden = new_hidden
+        classification = self.classifier(new_hidden)
+        return classification
+    
+    def trainable_parameters(self):
+        return filter(lambda p: p.requires_grad,
+            super(AnomalyModel, self).parameters())
+
+    def use_cuda(self):
+        return use_cuda
